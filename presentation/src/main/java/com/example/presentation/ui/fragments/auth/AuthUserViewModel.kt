1b014947d719c57a2b868ed003de585e83f83935
jodar <zodarbekn@gmail.com>
2023-06-08 18:50:30 +0300

    убрал getUser

--- presentation/src/main/java/com/example/presentation/ui/fragments/auth/AuthUserViewModel.kt
+++ presentation/src/main/java/com/example/presentation/ui/fragments/auth/AuthUserViewModel.kt
@@ -2,17 +2,12 @@ package com.example.presentation.ui.fragments.auth
 
 import androidx.lifecycle.ViewModel
 import com.example.domain.usecases.AuthUserUseCase
-import com.example.domain.usecases.GetUserUseCase
-import com.example.presentation.models.UserUI
-import com.example.presentation.models.toUI
 import dagger.hilt.android.lifecycle.HiltViewModel
 import javax.inject.Inject
 
 @HiltViewModel
 class AuthUserViewModel @Inject constructor(
     private val authUserUseCase : AuthUserUseCase,
-    private  val getUserUseCase : GetUserUseCase
-
 ): ViewModel() {
 
     fun authUser(name : String, surname : String, age : Int){
